d67cde764fb7f0f6cd5d1cc711011cabf0fa7a49
ArnaudDelanoue <Arnaud.delanoue@gmail.com>
2016-06-20 17:59:27 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ Copyright Oracle..
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
+public class HelloWorldX {
   public static void main(String[] args) {
     System.out.println("Hello World, my name is Bob");
     System.out.println("How are you?");
